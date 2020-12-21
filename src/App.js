8a37e2112106ed42588eceeb16aa85019744dbff
benjy <ramet.benj@gmail.com>
2020-12-21 02:00:05 +0100

    nav+framer motion

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ import GlobalStyle from './components/GlobalStyle';
 
 //import pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import MovieDetail from './pages/MovieDetail';
+
+//import nav
+import Nav from './components/Nav';
+
+//router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
